d6fb7103596ed577f2936412725dc5bd8e073592
CiaShangLin <nick034po@yahoo.com.tw>
2021-04-24 00:10:20 +0800

    2021/4/24

---
+++ app/src/main/java/com/shang/cuimusicplayer/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.shang.cuimusicplayer
+
+import android.app.Application
+import androidx.multidex.MultiDexApplication
+
+class MyApplication : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
